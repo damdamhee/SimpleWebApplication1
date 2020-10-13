cd2f1d9fd39fde6af224420866c14929614d9738
JinHyung Jung <jh9310s@gmail.com>
2020-10-13 19:33:43 +0900

    [추가] main.js
    - 화면에 출력되는 테이블에 표시될 데이터 자료구조 정의 및 데이터 하드코딩

---
+++ src/main.js
@@ -0,0 +1,7 @@
+//data
+var tableHeaders = ["Name", "Phone No.", "Address"];
+var listOfPeople = [
+                     {name:'JJH',phno:'010-1234-5678', addr:'suwon'},
+                     {name:'JHJ', phno:'010-0987-6543', addr:'suwon'}
+                    ];
+
