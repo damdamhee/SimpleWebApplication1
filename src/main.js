bc14115db51c587bde9b183bbf7c7f389dd3b3e3
JinHyung Jung <jh9310s@gmail.com>
2020-10-13 21:04:23 +0900

    [수정] 모듈화
    - 기존에 main.js에 정의되었던 변수 및 함수를 별도의 모듈로 분리

--- src/main.js
+++ src/main.js
@@ -1,69 +1,15 @@
-//data
-var tableHeaders = ["Name", "Phone No.", "Address"];
-var listOfPeople = [
-                     {name:'JJH',phno:'010-1234-5678', addr:'suwon'},
-                     {name:'JHJ', phno:'010-0987-6543', addr:'suwon'}
-                    ];
+import useEntry from "./useEntry.js";
+import useHeader from "./useHeader.js";
+import useDivider from "./useDivider.js";
+import { usePeopleTable, useAddPersonBtn } from "./usePeopleTable.js";
 
-//doms
-const entryDiv = document.getElementById('main');
-
-const header = document.createElement('h1');
-header.innerText = "List of people in DST"
-
-const divider = document.createElement('div');
-divider.style.height="30px";
-
-const peopleTable = document.createElement('table');
-function addTableItemRow(person){
-    let newItemRow = document.createElement('tr');
-    newItemRow.style.height="40px"
-    for(let col of Object.keys(person)){
-        let item = document.createElement('td');
-        let inputElement = document.createElement('input');
-        inputElement.value = person[col];
-        item.appendChild(inputElement);
-        newItemRow.appendChild(item);
-    } 
-    peopleTable.appendChild(newItemRow);
-}
-function renderTableHeader(){
-    let headerRow = document.createElement("tr");
-    headerRow.style.height="40px"
-    for(let header of tableHeaders){
-        let headerCol = document.createElement('th');
-        headerCol.innerText = header;
-        headerRow.appendChild(headerCol);
-    }
-    peopleTable.appendChild(headerRow);
-}
-function renderTableItems(){
-    for(let person of listOfPeople){
-        addTableItemRow(person);
-    }
-}
-
-const addPersonBtn = document.createElement('button');
-addPersonBtn.innerText = "ADD"
-addPersonBtn.setAttribute('id', 'addUserBtn');
-addPersonBtn.style.width="120px";
-addPersonBtn.style.height="40px";
-addPersonBtn.style.color="white";
-addPersonBtn.style.backgroundColor="#55ACED";
-addPersonBtn.style.border="none";
-addPersonBtn.padding="15px 30px";
-addPersonBtn.fontSize="20px";
-addPersonBtn.cursor="pointer";
-addPersonBtn.addEventListener('click', function(){
-    let newPerson = {name:"", phno:"", addr:""}
-    listOfPeople.push(newPerson);
-    addTableItemRow(newPerson);
-})
+const entryDiv = useEntry();
+const header = useHeader();
+const divider = useDivider();
+const peopleTable = usePeopleTable();
+const addPersonBtn = useAddPersonBtn();
 
 entryDiv.appendChild(header);
 entryDiv.appendChild(addPersonBtn);
 entryDiv.appendChild(divider);
 entryDiv.appendChild(peopleTable);
-
-renderTableHeader();
-renderTableItems();
