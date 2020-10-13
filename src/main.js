942a9ed1ca25671099ea924007ffd667ab351981
JinHyung Jung <jh9310s@gmail.com>
2020-10-13 19:51:46 +0900

    [수정] main.js
    - header 엘리먼트 정의
    - divider 엘리먼트 정의
    - peopleTable 엘리먼트 정의
    - peopleTable 엘리먼트 조작 관련 함수 정의

--- src/main.js
+++ src/main.js
@@ -7,3 +7,46 @@ var listOfPeople = [
 
 //doms
 const entryDiv = document.getElementById('main');
+
+const header = document.createElement('h1');
+header.innerText = "List of people in DST"
+
+const divider = document.createElement('div');
+divider.style.height="30px";
+
+const peopleTable = document.createElement('table');
+function addTableItemRow(person){
+    let newItemRow = document.createElement('tr');
+    newItemRow.style.height="40px"
+    for(let col of Object.keys(person)){
+        let item = document.createElement('td');
+        let inputElement = document.createElement('input');
+        inputElement.value = person[col];
+        item.appendChild(inputElement);
+        newItemRow.appendChild(item);
+    } 
+    peopleTable.appendChild(newItemRow);
+}
+function renderTableHeader(){
+    let headerRow = document.createElement("tr");
+    headerRow.style.height="40px"
+    for(let header of tableHeaders){
+        let headerCol = document.createElement('th');
+        headerCol.innerText = header;
+        headerRow.appendChild(headerCol);
+    }
+    peopleTable.appendChild(headerRow);
+}
+function renderTableItems(){
+    for(let person of listOfPeople){
+        addTableItemRow(person);
+    }
+}
+
+
+entryDiv.appendChild(header);
+entryDiv.appendChild(divider);
+entryDiv.appendChild(peopleTable);
+
+renderTableHeader();
+renderTableItems();
